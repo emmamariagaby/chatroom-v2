5e55176e5fc990283a9eb860bcb777a57519caf3
Emma Andersson <Emma@Emmas-MacBook-Air-2.local>
2021-03-24 23:05:50 +0100

    Fix disconnect method on socket to solve page reload and exits

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ const LEAVE_MESSAGE = " has left the room!";
 const AUTH_FAILED_MESSAGE = "Password authentication failed";
 const AUTH_FAILED = "auth failed";
 const CONNECTION = "connection";
+const DISCONNECT = "disconnect";
 const JOIN_SUCCESSFUL = "join successful";
 const MESSAGE = "message";
 const UPDATE_CHAT = "update chat";
 const UPDATE_LIST = "update list";
 const JOIN_ROOM = "join room";
+const LEAVE_ROOM = "leave room";
 
 let rooms = [
     { id: 'admin', members: 0, password: '123' },
@@ -51,11 +53,11 @@ io.on(CONNECTION, (socket) => {
             socket.join(data.room.id, () => {
                 io.to(socket.id).emit(JOIN_SUCCESSFUL, data.room.id)
                 socket.username = data.username;
-                socket.room = data.room;
+                socket.room = data.room.id;
                 io.to(data.room.id).emit(UPDATE_CHAT, { username: socket.username, message: JOIN_MESSAGE })
                 const index = rooms.findIndex(room => room.id === data.room.id);
                 rooms[index].members++;
-                console.log(data.username + " successfully joined " + data.room.id);
+                console.log(data.username + " successfully joined: " + data.room.id);
             });
 
         } else {
@@ -65,7 +67,7 @@ io.on(CONNECTION, (socket) => {
                     socket.join(data.room.id, () => {
                         io.to(socket.id).emit(JOIN_SUCCESSFUL, data.room.id)
                         socket.username = data.username;
-                        socket.room = data.room;
+                        socket.room = data.room.id;
                         io.to(data.room.id).emit(UPDATE_CHAT, { username: socket.username, message: JOIN_MESSAGE })
                         const index = rooms.findIndex(room => room.id === data.room.id);
                         rooms[index].members++;
@@ -79,18 +81,18 @@ io.on(CONNECTION, (socket) => {
                 socket.join(data.room.id, () => {
                     io.to(socket.id).emit(JOIN_SUCCESSFUL, data.room.id)
                     socket.username = data.username;
-                    socket.room = data.room;
+                    socket.room = data.room.id;
                     io.to(data.room.id).emit(UPDATE_CHAT, { username: socket.username, message: JOIN_MESSAGE })
                     const index = rooms.findIndex(room => room.id === data.room.id);
                     rooms[index].members++;
-                    console.log(data.username + " successfully joined " + data.room.id);
+                    console.log(data.username + " successfully joined: " + data.room.id);
                 });
             }
         }
         io.sockets.emit(UPDATE_LIST);
     });
 
-    socket.on("leave room", (data) => {
+    socket.on(LEAVE_ROOM, (data) => {
         console.log(socket.username + " left room: " + data.id + ", syncing rooms")
         rooms.forEach((room) => {
             if (room.id === data.id) {
@@ -104,6 +106,24 @@ io.on(CONNECTION, (socket) => {
                 io.sockets.emit(UPDATE_LIST);
             }
         });
+    });
+
+    socket.on(DISCONNECT, () => {
+        if (socket.room != undefined) {
+            console.log(socket.username + " disconnected from: " + socket.room + ", syncing rooms")
+            rooms.forEach((room) => {
+                if (room.id === socket.room) {
+                    room.members--;
+                    io.to(socket.room).emit(UPDATE_CHAT, { username: socket.username, message: LEAVE_MESSAGE })
+                    if (room.members < 1) {
+                        if (room.id != "admin" && room.id != "general") {
+                            rooms.splice(rooms.findIndex(room => room.id === socket.room), 1);
+                        }
+                    }
+                    io.sockets.emit(UPDATE_LIST);
+                }
+            });
+        }
     });
 
     socket.on(MESSAGE, (data) => {
